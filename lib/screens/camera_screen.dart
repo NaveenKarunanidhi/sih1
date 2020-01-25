0c7cc0a934a1e8b582b86dd65d6e4b038064971d
NAVEEN.K <45743172+NaveenKarunanidhi@users.noreply.github.com>
2020-01-25 20:22:11 +0530

    Update camera_screen.dart

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,18 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login_ui/screens/drive.dart';
 
-class CameraScreen extends StatelessWidget {
+import 'drawer.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => new _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    var spacecrafts = ["AREA1","AREA2","AREA3","AREA4","AREA5","AREA6","AREA7","AREA8","AREA9","AREA10"];
+    var myGridView = new GridView.builder(
+      itemCount: spacecrafts.length,
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (BuildContext context, int index) {
+        return new GestureDetector(
+          child: new Card(
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            elevation: 5.0,
+            margin: EdgeInsets.all(8.0),
+            child: new Container(
+              
+              alignment: Alignment.centerLeft,
+              margin: new EdgeInsets.fromLTRB(65.0, 140.0, 16.0, 8.0),
+              child: new Text(spacecrafts[index]),
+              decoration: new BoxDecoration(
+                image: DecorationImage(
+                  image: new AssetImage('assets/logos/imgicon9.png'),
+                  fit: BoxFit.contain,
+
+                 )
+              ),
+            ),
+          ),
+            onTap: () {
+             Navigator.of(context).push(MaterialPageRoute<Null>(builder: (BuildContext)
+          {
+            return new DriveScreen();
+          }));
+          },
+        );
+      },
+    );
+     return new Scaffold(
+     appBar: AppBar(
         leading: IconButton(
           icon: Icon(
             Icons.menu,
             semanticLabel: 'menu',
           ),
-          onPressed: (){
-            print('Menu');
-          },
+         onPressed: ()
+        {
+          Navigator.of(context).push(MaterialPageRoute<Null>(builder: (BuildContext)
+          {
+            return new DrawScreen();
+          }));
+        },
         ),
         title: Text('CAMERAS'),
         actions: <Widget>[
@@ -38,62 +84,13 @@ class CameraScreen extends StatelessWidget {
           ), 
         ],
       ),
-      body: GridView.count(
-  crossAxisCount: 2,
-  padding: EdgeInsets.all(16.0),
-  childAspectRatio: 8.0 / 9.0,
-  children: <Widget>[
-  Card(
-    clipBehavior: Clip.antiAlias,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        AspectRatio(
-          aspectRatio: 18.0 / 11.0,
-          child: Image.asset('assets/logos/imgicon9.png'),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('AREA 1'),
-              SizedBox(height: 8.0),
-              Text('North East'),
-              Card(
-    clipBehavior: Clip.antiAlias,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: <Widget>[
-        AspectRatio(
-          aspectRatio: 18.0 / 11.0,
-          child: Image.asset('assets/logos/imgicon9.png'),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(60.0, 7.0, 45.0, 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Text('AREA 2'),
-              SizedBox(height: 8.0),
-              Text('South East'),
-            ],
-          ),
-        ),
-      ],
-    ),
-
-  )
-],
-),
-
-),
-      ],
-    ),
-  )
-  ],
-      ),
+      body: myGridView,
     );
+  }
+}
+          
 
-}
-}
+
+ 
+
+
